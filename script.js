8e08a6641732f5b8d078708328144b469f4ccbbb
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2024-01-11 19:59:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const parseCode = (str) => {
 };
 
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+//const str = prompt("Enter str: ");
+//alert(JSON.stringify(parseCode(str)));
