6d632253d0d06df7157b995d8972d52214d65d5f
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2024-01-11 20:00:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const parseCode = (str) => {
-  // your code here
+	let splited = (str.split(0))
+	let splited1 = [],obj={};
+	//console.log(splited)
+	for(let i=0;i<splited.length;i++){
+		if(splited[i].length>1){
+			splited1.push(splited[i]);
+		}
+	}
+	//console.log(splited1);
+	let keys = ["firstName", "lastName", "id"]
+	for (let i=0;i<keys.length;i++){
+		obj[keys[i]]=splited1[i];
+	}
+	console.log(obj);
 };
 
 // Do not change the code below
